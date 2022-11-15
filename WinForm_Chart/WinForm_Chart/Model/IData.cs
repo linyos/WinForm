31398a50da1e25b5d4bb36e12ebbad717ff88f32
sen <laso85143@gmail.com>
2022-11-16 01:35:26 +0800

    Add form2

---
+++ WinForm_Chart/WinForm_Chart/Model/IData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WinForm_Chart.Model
+{
+    public  interface IData
+    {
+        [Description("id")]
+        public int Id { get; set; }
+        
+        [Description("FirstName")]
+        public string Firstname { get; set; }
+        [Description("LastName")]
+        public string Lastname { get; set; }
+        [Description("City")]
+        public string City { get; set; }
+    }
+ 
+}
---
+++ WinForm_Chart/WinForm_Chart/service/Method.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Text.Json;
+using WinForm_Chart.Model;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinForm_Chart.service
+{
+   public static class Method
+    {
+
+
+        public static List<Data> ReadJson(string path )
+        {
+            List<Data> source = new List<Data>();
+            //string path = @"C:\SEN\Coding\C#\WinForm\WinForm_Chart\WinForm_Chart\test.json";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string jsonText = reader.ReadToEnd();
+                source = System.Text.Json.JsonSerializer.Deserialize<List<Data>>(jsonText);
+            }
+            return source;
+        }
+    }
+}
